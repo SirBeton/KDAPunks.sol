9b0873256af3bd96904ebf67411e5e43341599ec
The Mod Father Beton <32620189+SirBeton@users.noreply.github.com>
2022-01-06 07:56:13 +0000

    Update token.sol

--- token.sol
+++ token.sol
@@ -14,7 +14,7 @@
 
    #KDAPunks features:
    3% fee auto add to the liquidity pool to locked forever when selling
-   2% fee auto distribute to all holders
+   4% fee auto distribute to all holders
    I created a black hole so #Bee token will deflate itself in supply with every transaction
    50% Supply is burned at start.
    
